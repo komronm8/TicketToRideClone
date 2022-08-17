bbc86e22bfdb1bae22d922d1879c6718316b4222
Komron <komron.abdulloev@tu-dortmund.de>
2022-08-17 20:45:51 +0200

    Added some methods/variables for the menu scenes

--- src/main/kotlin/view/SopraApplication.kt
+++ src/main/kotlin/view/SopraApplication.kt
@@ -3,11 +3,42 @@ package view
 import service.GameService
 import service.RootService
 import tools.aqua.bgw.core.BoardGameApplication
+//import tools.aqua.bgw.core.WindowMode(for fullscreen)
 
 class SopraApplication : BoardGameApplication("Zug um Zug"), Refreshable {
-    private val rootService: RootService = RootService()
+
+    //If wanted, we can make window fullscreen, by adding "windowMode = WindowMode.FULLSCREEN"
 
     private var gameScene: GameScene
+
+    private val rootService: RootService = RootService()
+
+    private var configScene = ConfigPlayerScene(rootService).apply {
+        goBackButton.onMouseClicked = {
+            goBack()
+            this.remove()
+        }
+    }
+
+    private var endScene = EndGameScene(rootService)
+
+    private var mainMenuScene = NewGameScene(rootService).apply {
+        soloButton.onMouseClicked = {
+            configScene.addSoloComponents()
+            this@SopraApplication.hideMenuScene()
+            this@SopraApplication.showMenuScene(configScene)
+        }
+
+        onlineButton.onMouseClicked = {
+            configScene.addOnlineComponents()
+            this@SopraApplication.hideMenuScene()
+            this@SopraApplication.showMenuScene(configScene)
+        }
+
+        closeButton.onMouseClicked = {
+            exit()
+        }
+    }
 
     init {
         rootService.gameService.startNewGame(listOf(
@@ -16,5 +47,12 @@ class SopraApplication : BoardGameApplication("Zug um Zug"), Refreshable {
             GameService.PlayerData("c", false)))
         gameScene = GameScene(rootService)
         this.showGameScene(gameScene)
+
+//        this.showMenuScene(mainMenuScene)
     }
+
+    private fun goBack(){
+        this.showMenuScene(mainMenuScene)
+    }
+
 }
