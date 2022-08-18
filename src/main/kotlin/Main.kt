7af147d5cae74febc1f5b283b1fb1e827501cf56
Komron <komron.abdulloev@tu-dortmund.de>
2022-08-19 00:07:05 +0200

    Commented AIService.minMaxAIGame() out from Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ import service.AIService
 import view.SopraApplication
 
 fun main() {
-    AIService.minMaxAIGame()
-    //SopraApplication().show()
+    //AIService.minMaxAIGame()
+    SopraApplication().show()
     println("Application ended. Goodbye")
 }
