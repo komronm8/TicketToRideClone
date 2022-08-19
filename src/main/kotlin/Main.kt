cb24ce03e18f069658eb468c434291d3f82ec6f2
oliver2.mueller <Minecraft2001>
2022-08-19 09:26:43 +0200

    kurzes Main update für Network Tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,20 @@
 import service.AIService
+import service.NetworkService
+import service.RootService
 import view.SopraApplication
 
 fun main() {
     //AIService.minMaxAIGame()
-    SopraApplication().show()
+    //SopraApplication().show()
+    mainNetwork()
     println("Application ended. Goodbye")
+}
+
+/**
+ * Function for Testing Network Features
+ */
+fun mainNetwork(){
+    val root = RootService()
+    val game = NetworkService(root)
+    game.hostGame("net22c", "TestGroup06", "GROUP6FTW")
 }
