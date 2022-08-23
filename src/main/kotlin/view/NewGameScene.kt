ecff3e04e2f174b92675cee52d1790ca44790b5e
Matti Krause <noemail>
2022-08-23 17:05:53 +0200

    changed paths

--- src/main/kotlin/view/NewGameScene.kt
+++ src/main/kotlin/view/NewGameScene.kt
@@ -11,18 +11,18 @@ import tools.aqua.bgw.components.uicomponents.Button
  * There is also a [closeButton]  to end the program.
  */
 class NewGameScene(private val rootService: RootService):
-    MenuScene(1920, 1080, ImageVisual("\\MenuScene\\MainMenuScreen.png")), Refreshable{
+    MenuScene(1920, 1080, ImageVisual("MenuScene/MainMenuScreen.png")), Refreshable{
 
     val soloButton = Button(
-        posX = 810, posY = 420, width = 315, height = 138, visual = ImageVisual("\\MenuScene\\soloButton.png")
+        posX = 810, posY = 420, width = 315, height = 138, visual = ImageVisual("MenuScene/soloButton.png")
     )
 
     val onlineButton = Button(
-        posX = 810, posY = 550, width = 315, height = 138, visual = ImageVisual("\\MenuScene\\onlineButton.png")
+        posX = 810, posY = 550, width = 315, height = 138, visual = ImageVisual("MenuScene/onlineButton.png")
     )
 
     val closeButton = Button(
-        posX = 1770, posY = 10, width = 141, height = 143, visual = ImageVisual("\\MenuScene\\closeButton.png")
+        posX = 1770, posY = 10, width = 141, height = 143, visual = ImageVisual("MenuScene/closeButton.png")
     )
 
     init{
