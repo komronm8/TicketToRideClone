18bb652ac299ae6fbbad0cc2b9c6df6c376b96e2
Matti Krause <noemail>
2022-08-23 17:48:52 +0200

    added id to Map.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 import service.NetworkService
 import service.RootService
-import view.SopraApplication
 
 
 fun main() {
     //AIService.minMaxAIGame()
-    SopraApplication().show()
+    //SopraApplication().show()
     //mainNetwork()
+
     println("Application ended. Goodbye")
 }
 
