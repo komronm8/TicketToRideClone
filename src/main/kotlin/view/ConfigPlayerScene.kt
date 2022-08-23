c507150a327f4c77a8c059d9210ba30a09b272c0
Komron <komron.abdulloev@tu-dortmund.de>
2022-08-23 15:11:15 +0200

    Update for refreshable made

--- src/main/kotlin/view/ConfigPlayerScene.kt
+++ src/main/kotlin/view/ConfigPlayerScene.kt
@@ -457,6 +457,7 @@ class ConfigPlayerScene(private val rootService: RootService):
         val listOfPlayers = rootService.network.client?.playersNames
         checkNotNull(listOfPlayers)
         println("REFRESHABLE CALLED!")
+        removeComponents(player1LobbyLabel, player2LobbyLabel, player3LobbyLabel)
         player1LobbyLabel.text = "Player1: " + listOfPlayers[0]
         addComponents(player1LobbyLabel)
         if(listOfPlayers.size >= 2){
